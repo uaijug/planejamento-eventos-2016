68eedf0d56f93ff8e3b3329757db09ddaf48ded0
Diego A. Silva <diego.silva.facom@gmail.com>
2016-02-16 14:34:43 -0200

    criação da apresentação padrão do UaiJUG

--- presentations/uaijug/uaijug-presentation/js/include.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    var includes = $('.include');
-    jQuery.each(includes, function(){
-      var file = 'slides/' + $(this).data('include');
-      $(this).load(file);
-    });
-});
